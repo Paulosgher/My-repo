47ed63d83e43ec0cb5ffe83baf9c2b787c17f958
paulos <106759008+Paulosgher@users.noreply.github.com>
2023-06-17 18:04:55 +0300

    Update Guess_random_num.js

--- Guess_random_num.js
+++ Guess_random_num.js
@@ -7,7 +7,7 @@ const prompt = r1();
 const machine = (y) => {
   const guess = prompt("guess anumber between 1-10: ");
   if(y==guess){
-    console.log("correctyou got it bu it is note Lottory bro!!!");
+    console.log("correct you got it bu it is note Lottory 😁!!!");
   }
   else{
     console.log("error try another number")
